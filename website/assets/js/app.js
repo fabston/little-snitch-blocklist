5b30616f8f43b06bf02f65b7764a8ea6918fbba1
fabston <fbstn@pm.me>
2022-03-29 19:30:57 +0700

    Added website

---
+++ website/assets/js/app.js
@@ -0,0 +1,15 @@
+const byID = id => document.getElementById(id);
+const TEST_URL = 'https://magical-cuchufli-329865.netlify.app?_=';
+const request = new XMLHttpRequest();
+
+request.open('GET', TEST_URL + new Date().getTime(), true);
+
+request.onload = function() {
+    byID('test-danger').classList.add('d-block');
+};
+
+request.onerror = function() {
+    byID('test-success').classList.add('d-block');
+};
+
+request.send();
