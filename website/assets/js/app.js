4c27d01b360eaa4c38c904d380ac13be6e06280c
fabston <fbstn@pm.me>
2023-03-31 19:02:50 +0700

    small fix

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,5 +1,5 @@
 const byID = id => document.getElementById(id);
-const TEST_URL = 'https://little-snitch-blocklist.netlify.app/test.html';
+const TEST_URL = 'https://little-snitch-blocklist.netlify.app/test.html?';
 const request = new XMLHttpRequest();
 
 request.open('GET', TEST_URL + new Date().getTime(), true);
